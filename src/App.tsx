53f7632940be641b61fb3600eb38063c015c3f1e
Filipe Zanelato <47918618+Filipex123@users.noreply.github.com>
2025-11-05 19:28:09 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -126,7 +126,7 @@ function App() {
           <div className="text-center w-full">
             <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-light text-gray-800 mb-8 md:mb-12 tracking-tight">Dra. Isabela Allegrini Zanelato</h1>
             <p className="text-base sm:text-lg md:text-xl text-gray-600 max-w-3xl mx-auto leading-relaxed px-4">
-              Cuidado personalizado com foco na saúde e beleza natural da sua pele, unindo ciência, tecnologia e uma abordagem humanizada.
+              Dermatologia Estética Integrativa
             </p>
             {/* <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-light text-gray-800 mb-8 md:mb-12 tracking-tight">Cuidar da pele é mais do que estética.<br />É restaurar equilíbrio, vitalidade e confiança.</h1>
             <p className="text-base sm:text-lg md:text-xl text-gray-600 max-w-3xl mx-auto leading-relaxed px-4">
